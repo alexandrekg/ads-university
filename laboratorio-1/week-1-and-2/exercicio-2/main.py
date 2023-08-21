d4b0e53c2170c239878a88f2d943ed6f8717d088
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-08-21 20:50:25 -0300

    2023/08/21 - Finish exercise 2

---
+++ laboratorio-1/week-1-and-2/exercicio-2/main.py
@@ -0,0 +1,9 @@
+
+def main():
+    nome = input("Digite seu nome... ")
+    idade = input("Digite sua idade... ")
+    print(f"Seu nome eh {nome} e voce tem {idade} anos.")
+
+
+if __name__ == "__main__":
+    main()
