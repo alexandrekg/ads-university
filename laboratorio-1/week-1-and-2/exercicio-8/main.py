372b25bd1a830ed8fdb71dce5bbf456271bc65f2
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-08-22 10:16:13 -0300

    2023/08/22 - Update exercise 8

--- laboratorio-1/week-1-and-2/exercicio-8/main.py
+++ laboratorio-1/week-1-and-2/exercicio-8/main.py
@@ -1,21 +1,5 @@
-
-
 def main():
-    nota_trabalho = int(input("Digite a nota do trabalho... "))
-    nota_prova = int(input("Digite a nota da prova... "))
-    nota_teste = int(input("Digite a nota do teste... "))
-    
-    peso_nota_trabalho = 0.1
-    peso_nota_prova = 0.6
-    peso_nota_teste = 0.3
-    
-    nota_representativa_trabalho = peso_nota_trabalho * nota_trabalho
-    nota_representativa_prova = peso_nota_prova * nota_prova
-    nota_representativa_teste = peso_nota_teste * nota_teste
-    
-    nota_final = nota_representativa_trabalho + nota_representativa_prova + nota_representativa_teste
-    
-    print(f"A nota final do aluno eh: {nota_final}")
+
 
 if __name__ == "__main__":
     main()
