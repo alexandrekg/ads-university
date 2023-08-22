d482636893d3ec0fc242c093ebfd18e04097907d
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-08-22 20:29:35 -0300

    2023/08/22 - Finish advanced exercise 2

---
+++ laboratorio-1/week-1-and-2/exercicio-avancado-2/main.py
@@ -0,0 +1,17 @@
+import math
+
+
+def main():
+    a = int(input("Digite o primeiro numero inteiro... "))
+    b = int(input("Digite o segundo numero inteiro... "))
+    c = int(input("Digite o terceiro numero inteiro... "))
+
+    raiz_discriminante = (b ** 2) - 4 * a * c 
+    raiz_positiva = - b + math.sqrt(raiz_discriminante) / (2 * a)
+    raiz_negativa = - b - math.sqrt(raiz_discriminante) / (2 * a)
+    
+    print(f"A raiz positiva eh: {raiz_positiva}")
+    print(f"A raiz negativa eh: {raiz_negativa}")
+
+if __name__ == "__main__":
+    main()
