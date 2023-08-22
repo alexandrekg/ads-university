dcda218da4a75457bbbf19d75644b57033959dc5
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-08-21 21:28:21 -0300

    2023/08/21 - Finish exercise 5

--- laboratorio-1/week-1-and-2/exercicio-5/main.py
+++ laboratorio-1/week-1-and-2/exercicio-5/main.py
@@ -1,14 +1,17 @@
 
 def main():
-    nome_rua = input("Digite sua rua... ")
-    cep = input("Digite seu CEP (Exemplo 91444-221) ... ")
-    complemento = input("Digite seu complemento... ")
-    bairro = input("Digite seu bairro... ")
-    estado = input("Digite seu estado... ")
-    cidade = input("Digite sua cidade... ")
+    numero1 = int(input("Digite o numero 1... "))
+    numero2 = int(input("Digite o numero 2... "))
+    numero3 = int(input("Digite o numero 3... "))
+    numero4 = int(input("Digite o numero 4... "))
+    numero5 = int(input("Digite o numero 5... "))
     
-    print(f"O endereco fica na rua {nome_rua}, {complemento}, CEP {cep} - {bairro}. {cidade}/{estado}")
-
+    soma = numero1 + numero2 + numero3 + numero4 + numero5
+    produto = numero1 * numero2 * numero3 * numero4 * numero5
+    
+    print(f"A soma dos numeros eh: {soma}")
+    print(f"O produto dos numeros eh: {produto}")
+    
 
 if __name__ == "__main__":
     main()
