cb0f987ecdf09745f39b7bcdd53727649e562b25
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-08-29 10:20:02 -0300

    2023/08/29 - finalizando exercicio 14

---
+++ programacao-1/week-3-and-4/exercicio-14/main.py
@@ -0,0 +1,11 @@
+
+
+def main():
+    numero = int(input("Digite um numero inteiro... "))
+    
+    for i in range(0, 11):
+        print(f"{numero} x {i} = {numero * i}")
+    
+
+if __name__ == "__main__":
+    main()
---
+++ programacao-1/week-3-and-4/exercicio-14/notas.md
@@ -0,0 +1,3 @@
+# Exercicio 14
+
++ Utilizando for, crie um programa que imprime a tabuada de um numero inteiro pelo usuario
