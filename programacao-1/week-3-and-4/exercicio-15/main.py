fcaab3bc53d258bf6da6571a6e2b7bb34b5a6e03
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-08-29 10:26:16 -0300

    2023/08/29 - finalizando exercicio 15

--- programacao-1/week-3-and-4/exercicio-15/main.py
+++ programacao-1/week-3-and-4/exercicio-15/main.py
@@ -1,11 +1,17 @@
 
 
 def main():
-    numero = int(input("Digite um numero inteiro... "))
+    qtd_produtos = int(input("Quantos produtos deseja adicionar na lista? "))
+    lista_produtos = []
     
-    for i in range(0, 11):
-        print(f"{numero} x {i} = {numero * i}")
-    
+    for i in range(0, qtd_produtos):
+        produto = input("Digite o produto que deseja adicionar na lista:  ")
+        lista_produtos.append(produto)        
+
+    print("Segue a sua lista de produtos: ")
+    for produto in lista_produtos:
+        print(f"- {produto}")
+        
 
 if __name__ == "__main__":
     main()
