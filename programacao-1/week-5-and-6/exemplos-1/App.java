c146ab532e5331827c551dcd87919959451d45cf
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-09-12 21:29:41 -0300

    2023/09/12 - adicionando exemplo 1 de Java

---
+++ programacao-1/week-5-and-6/exemplos-1/App.java
@@ -0,0 +1,7 @@
+package meuprojetoteste1;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Ola mundo!")
+    }
+}
---
+++ programacao-1/week-5-and-6/exemplos-1/Pessoa.java
@@ -0,0 +1,8 @@
+package meuprojetoteste1;
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+    private double altura;
+    private char sexo;
+}
