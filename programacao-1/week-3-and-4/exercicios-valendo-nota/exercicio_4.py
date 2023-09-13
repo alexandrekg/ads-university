8dc34cff422cbc47d1be5882b5d384569465244c
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-09-13 07:05:13 -0300

    2023/09/13 - Adicionando Programacao1/Week-3-and-4/Exercicio4valendo-nota

---
+++ programacao-1/week-3-and-4/exercicios-valendo-nota/exercicio_4.py
@@ -0,0 +1,29 @@
+"""
+Considere a linha de código (em Python) abaixo:
+x = "Até a pé nós iremos"
+Sabendo que uma string é uma lista (em que cada posição da lista possui um dos caracteres da string), assinale a alternativa que apresenta um trecho de código que imprime cada um dos caracteres desta string x um abaixo do outro, da seguinte forma:
+A
+t
+é
+ 
+a
+ 
+p
+é
+ 
+n
+ó
+s
+ 
+i
+r
+e
+m
+o
+s
+"""
+
+x = "Até a pé nós iremos"
+
+for i in x:
+    print(i)
