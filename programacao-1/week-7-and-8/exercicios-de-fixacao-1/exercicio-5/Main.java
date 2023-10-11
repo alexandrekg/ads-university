e7cb287a11c2fa9650848edb1b2e6161f5c0c252
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-10-10 22:15:43 -0300

    2023/10/10 - Finalizando exercicio 5

---
+++ programacao-1/week-7-and-8/exercicios-de-fixacao-1/exercicio-5/Main.java
@@ -0,0 +1,12 @@
+package meuprojetoteste1;
+
+import meuprojetoteste1.Carro;
+
+public class App {
+
+	public static void main(String[] args) {
+		Carro c = new Carro("Fiat");
+		System.out.println(c.getMarca());
+	}
+
+}
