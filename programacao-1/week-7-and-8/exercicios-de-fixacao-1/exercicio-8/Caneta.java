8e6fd69ce39abfda3edf8eb35de03aae6ebbcf21
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-10-11 21:41:58 -0300

    2023/10/11 - Criando classes Estojo, Cor e Caneta

---
+++ programacao-1/week-7-and-8/exercicios-de-fixacao-1/exercicio-8/Caneta.java
@@ -0,0 +1,17 @@
+package meuprojetoteste1;
+
+public class Caneta {
+	private String caneta;
+	
+	public Caneta(int caneta) {
+        this.caneta = caneta;
+    }
+
+    public int getCaneta(){
+        return caneta;
+    }
+
+    public void setCaneta(int caneta){
+        this.caneta = caneta;
+    }
+}
