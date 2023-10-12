3fcc7cd4359b83b19c525033c5cbb5a3900ee467
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-10-11 21:56:56 -0300

    2023/10/11 - Atualizando notas

--- programacao-1/week-7-and-8/exercicios-de-fixacao-1/exercicio-8/Estojo.java
+++ programacao-1/week-7-and-8/exercicios-de-fixacao-1/exercicio-8/Estojo.java
@@ -1,17 +1,40 @@
 package meuprojetoteste1;
 
-public class Pessoa {
-	private int idade;
+import meuprojetoteste1.caneta;
+import meuprojetoteste1.Caneta;
+
+public class Estojo {
+	private Cor cor;
+    private Caneta caneta1;
+    private Caneta caneta2;
 	
-	public Pessoa(int idade) {
-        this.idade = idade;
+	public Estojo(int cor, caneta1, caneta2) {
+        this.cor = cor;
+        this.caneta1 = caneta1;
+        this.caneta2 = caneta2;
     }
 
-    public int getIdade(){
-        return idade;
+    public Cor getCor(){
+        return cor;
     }
 
-    public void setIdade(int idade){
-        this.idade = idade;
+    public void setCor(Cor cor){
+        this.cor = cor;
+    }
+
+    public Caneta getCaneta1(){
+        return caneta1;
+    }
+
+    public void setCaneta1(Caneta caneta1){
+        this.caneta = caneta;
+    }
+
+    public Caneta getCaneta2(){
+        return caneta2;
+    }
+
+    public void setCaneta2(Caneta caneta2){
+        this.caneta = caneta;
     }
 }
